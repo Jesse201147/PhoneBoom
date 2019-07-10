78dbb6d6c512c5d28365cc9abd612d9dd44ee35c
Jesse.T <tjx201147@163.com>
2019-07-10 15:16:38 +0800

    'init'

---
+++ PhoneBoom/config.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# @Author : Jesse.T
+# @Time   : 2019-07-10 13:59
+website = [
+    dict(
+        website='易法通',
+        url='http://m.yifatong.com/Customers/register',
+        jscode='''
+                document.getElementById('CustomerAccount').value='{}';
+                document.getElementById('btn-sms').click();
+                '''
+    ),
+    # dict(
+    #     website='快易出行企业管理平台',
+    #     url='https://ems.xg-yc.com/#/login',
+    #     jscode='''
+    #         function timedMsg(){
+    #         　　var t=setTimeout("document.querySelector('div.el-form-item input').value='13680260549';document.querySelector('button.btn').click()",5000)
+    #         　　              };
+    #         document.querySelector('ul.nav>li:nth-child(2)>a').click();
+    #         timedMsg();
+    #         '''
+    # ),
+    # dict(
+    #     website='天津市企业登记全程电子化服务平台',
+    #     url='https://ems.xg-yc.com/#/login',
+    #     jscode='''
+    #         document.querySelector('button.zhuce').click();
+    #         document.querySelector('#upRphone1').value='{}';
+    #         document.querySelector('#upRfsyzm1').click();
+    #         '''
+    # ),
+    # dict(
+    #     website='爱四级商城',
+    #     url='https://www.i4season.com/html/mall/register.html',
+    #     jscode='''
+    #         document.getElementById('register_phone').value='{}';
+    #         document.getElementById('mobile_id').click();
+    #         '''
+    # ),
+    dict(
+        website='敢尚商城',
+        url='http://www.gsall.cn/webApi/register',
+        jscode='''
+            document.getElementById('iphone').value='{}';
+            document.querySelector('input.fr.yzm_btn').click();
+            '''
+    ),
+    # dict(
+    #     website='快乐购商城',
+    #     url='https://www.happigo.com/register/',
+    #     jscode='''
+    #         document.querySelector('#mobileregistermodel-mobile').value='{}';
+    #         document.querySelector('#password').value='123@456@';
+    #         document.querySelector('a.phonecode').click();
+    #         '''
+    # ),
+]
---
+++ PhoneBoom/engine.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# @Author : Jesse.T
+# @Time   : 2019-07-10 9:44
+import asyncio
+import itertools
+import traceback
+
+from PhoneBoom.config import *
+from logzero import logger
+from pyppeteer import launch
+
+headless = True
+browser = None
+numbers = [123,]
+
+
+async def init(headless=headless):
+    global browser
+    logger.info('start task init Browser')
+    browser = await launch({'headless': headless})
+    logger.info('Browser inited')
+
+
+async def main(num, website, url, jscode, **kws):
+    global browser
+    logger.info(f'start task: {website}--{num}')
+    context = await browser.createIncognitoBrowserContext()
+    try:
+        page = await context.newPage()
+        await page.goto(url)
+        await asyncio.sleep(0.5)
+        await page.evaluate(jscode.format(num))
+        await asyncio.sleep(0.2)
+        logger.info(f'task: {website}--{num} succeed')
+    except:
+        print(traceback.format_exc())
+        logger.error(f'task: {website}--{num} failed')
+    finally:
+        await asyncio.sleep(10)
+        await context.close()
+
+
+async def close():
+    global browser
+    logger.info('task finished close Browser')
+    await browser.close()
+    logger.info('Browser closed')
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(asyncio.wait([init(), ]))
+    run_queues = list(itertools.chain(*[[main(num=number, **i) for i in website] for number in numbers]))
+    asyncio.get_event_loop().run_until_complete(asyncio.wait(run_queues))
+    asyncio.get_event_loop().run_until_complete(asyncio.wait([close(), ]))
